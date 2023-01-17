03c64d70345e44e4a3bc75f4390a3bd95fce0933
fawzi-shiyyab19 <fawziakef@icloud.com>
2023-01-17 22:52:47 +0300

    simplify

---
+++ .history/src/components/main/First/hello_20230117214823.js
@@ -0,0 +1,38 @@
+import { Button, FormLabel, TextField, Box, Grid,Typography  } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import * as React from "react";
+import { purple } from "@mui/material/colors";
+import Images from "../../../assets/first.png";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeight: 700,
+    fontSize: 50,
+  },
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+});
+
+function FirstProject() {
+  return (
+    <Grid container item gap={"3rem"} justifyContent="space-between" xs={12}>
+      <Grid container item md={6} xs={12} flexDirection='column' justifyContent={"center"} alignContent={"center"}>
+      <Typography variant="h4" gutterBottom>WE ARE YOUR TECHNOLOGY PARTNER</Typography>
+        <Typography align="center" >
+          An innovation inspired, software design and consultancy company for
+          the digital age.
+        </Typography >
+        <Button color="secondary" variant="contained">
+          Get Started
+        </Button>
+      </Grid>
+      <Grid item md={4} xs={12} component='img' src={Images} />
+    </Grid>
+  );
+}
+export default FirstProject;
